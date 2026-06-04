238c97ea103b0f3812138dc9c188ca1a8e2c5272
gabrielca5 <gabrielca5@al.insper.edu.br>
2026-06-04 12:12:26 +0000

    Update from https://github.com/insper-classroom/frontend-sasf-1/commit/c7953f95c16f8c2b827d6682d87a960009ae2a89

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,42 @@
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import WarningAmberRoundedIcon from '@mui/icons-material/WarningAmberRounded'
 import logoPng from '../assets/chicoLogo.png'
 import AuthLayout from '../components/AuthLayout'
 import BrandHeader from '../components/BrandHeader'
 import LoginForm from '../components/LoginForm'
-import { AuthAlert } from './ui'
+import Button from '../components/ui/button'
+import { PageDialog } from './ui'
 
 function LoginPage() {
   const location = useLocation()
   const registered = location.state?.registered
+  const [approvalDialogOpen, setApprovalDialogOpen] = useState(Boolean(registered))
+
+  useEffect(() => {
+    if (registered) setApprovalDialogOpen(true)
+  }, [registered])
 
   return (
     <AuthLayout ariaLabel="Login de acesso">
       <BrandHeader logoSrc={logoPng}/>
-      {registered && (
-        <AuthAlert severity="success">Conta criada com sucesso! Faça login para continuar.</AuthAlert>
-      )}
       <LoginForm />
+      <PageDialog
+        open={approvalDialogOpen}
+        onClose={() => setApprovalDialogOpen(false)}
+        title="Acesso pendente"
+        titleIcon={<WarningAmberRoundedIcon color="warning" />}
+        actions={
+          <Button variant="contained" onClick={() => setApprovalDialogOpen(false)}>
+            Entendi
+          </Button>
+        }
+      >
+        <Typography color="text.secondary">
+          Cadastro enviado. Aguarde a aprovação do administrativo para fazer login.
+        </Typography>
+      </PageDialog>
     </AuthLayout>
   )
 }
